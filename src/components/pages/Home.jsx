1b6b3d1e95dc02434e80f8698df4170465b04be4
darya <atomicleda@mail.ru>
2023-08-30 20:08:41 +0300

    release 0

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,28 @@
+// import React from 'react'
+
+// export default function Home() {
+//   return (
+//     <div>Home</div>
+//   )
+// }
 import React from 'react'
+import { Navbar, Nav, Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">My App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <div>Home</div>
+    </div>
   )
 }
